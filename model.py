5e36a9ccfbd8e7a916f8271c345f49c2524309e7
Jia-Yiiiii <17640079336@163.com>
2026-05-18 11:05:46 +0800

    优化模型forward，使用**kwargs适配BERT原生传参

--- model.py
+++ model.py
@@ -8,9 +8,8 @@ class BertWithDropout(nn.Module):
         self.bert = BertModel.from_pretrained(config.model_name)
         self.dropout = nn.Dropout(config.dropout_rate)
         self.classifier = nn.Linear(self.bert.config.hidden_size, config.num_classes)
-
-    def forward(self, input_ids, attention_mask):
-        out = self.bert(input_ids, attention_mask=attention_mask)
+    def forward(self, **kwargs):
+        out = self.bert(** kwargs)  
         x = self.dropout(out.pooler_output)
         logits = self.classifier(x)
         return logits
