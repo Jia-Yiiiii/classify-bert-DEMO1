0e14fa86175da43e4789da37aeaf4e4754be1f45
Jia-Yiiiii <17640079336@163.com>
2026-06-10 14:43:08 +0800

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,7 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-from utils import load_config
-config = load_config("configs/Bert_Config_exp1.json")
+
 class Demo1_Dataset(Dataset):
     def __init__(self, texts, labels, tokenizer, max_len):
         self.texts = texts
@@ -10,11 +9,9 @@ class Demo1_Dataset(Dataset):
         self.max_len = max_len
     def __len__(self):
         return len(self.texts)
-
     def __getitem__(self, idx):
         text = self.texts[idx]
         label = self.labels[idx]
-
         x = self.tokenizer(
             text,
             truncation=True,
@@ -32,20 +29,17 @@ def collate_fn(batch):
     all_ids = []
     all_masks = []
     all_labels = []
-
     for i in batch:
         all_ids.append(i[0])
         all_masks.append(i[1])
         all_labels.append(i[2])
-
     input_ids = torch.stack(all_ids, dim=0)
     masks = torch.stack(all_masks, dim=0)
     labels = torch.stack(all_labels, dim=0)
-
     return input_ids, masks, labels
 
-def Load_Demo1_Data(file_path):
 
+def Load_Demo1_Data(file_path):
     data = []
     with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
@@ -58,6 +52,7 @@ def Load_Demo1_Data(file_path):
                 data.append((text, label))
     return data
 
+
 def get_textslabels(data, label_id):
     texts = []
     labels = []
@@ -66,7 +61,7 @@ def get_textslabels(data, label_id):
         labels.append(label_id[i[1]])
     return texts, labels
 
-def Myloader(texts, labels, tokenizer, batch_size, shuffle=True):
-    dataset = Demo1_Dataset(texts, labels, tokenizer, max_len=config["max_len"])
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
 
+def Myloader(texts, labels, tokenizer, batch_size, max_len, shuffle=True):
+    dataset = Demo1_Dataset(texts, labels, tokenizer, max_len)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
