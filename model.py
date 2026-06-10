0fdb9e0f6c57017e84d2ee4289fcb96956b2d7d8
Jia-Yiiiii <17640079336@163.com>
2026-06-10 14:43:46 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,15 +2,16 @@ import torch
 import torch.nn as nn
 from transformers import BertModel
 
+
 class BertWithDropout(nn.Module):
-    def __init__(self, config):
+    def __init__(self, model_name, dropout_rate, num_classes):
         super().__init__()
-        self.bert = BertModel.from_pretrained(config.model_name)
-        self.dropout = nn.Dropout(config.dropout_rate)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, config.num_classes)
+        self.bert = BertModel.from_pretrained(model_name)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.classifier = nn.Linear(self.bert.config.hidden_size, num_classes)
+
     def forward(self, **kwargs):
-        out = self.bert(** kwargs)
+        out = self.bert(**kwargs)
         x = self.dropout(out.pooler_output)
         logits = self.classifier(x)
         return logits
-
