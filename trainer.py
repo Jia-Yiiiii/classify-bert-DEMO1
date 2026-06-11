0b67b8512c4f73887ce9ccd091f44ee5dea6b4b6
Jia-Yiiiii <17640079336@163.com>
2026-06-11 21:49:38 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt
 from data_process import Load_Demo1_Data, get_textslabels, Myloader
 from model import BertWithDropout
 from utils import set_seed
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
+
 
 class Trainer:
     def __init__(self, config):
@@ -27,25 +26,19 @@ class Trainer:
         self.id_label = None
         set_seed(config.get("seed", 42))
     def load_data(self):
-
         train_data = Load_Demo1_Data(self.config["data_path"])
         dev_data = Load_Demo1_Data(self.config["dev_path"])
         test_data = Load_Demo1_Data(self.config["test_path"])
-
-
         unique_labels = []
         for item in train_data:
             label = item[1]
             if label not in unique_labels:
                 unique_labels.append(label)
-
         self.label_id = {}
         self.id_label = {}
         for i in range(len(unique_labels)):
             self.label_id[unique_labels[i]] = i
             self.id_label[i] = unique_labels[i]
-
-
         self.train_texts, self.train_labels = get_textslabels(train_data, self.label_id)
         self.dev_texts, self.dev_labels = get_textslabels(dev_data, self.label_id)
         self.test_texts, self.test_labels = get_textslabels(test_data, self.label_id)
@@ -192,7 +185,8 @@ class Trainer:
         self.plot(all_labels, all_preds)
 
     def plot(self, truths, preds):
-
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
         name_map = {
             "news_story": "新闻故事",
             "news_culture": "新闻文化",
@@ -270,6 +264,7 @@ class Trainer:
         self.dataloader()
         self.init_model()
         self.train()
+
 
 if __name__ == "__main__":
     with open("configs/Bert_Config_exp1.json", "r", encoding="utf-8") as f:
