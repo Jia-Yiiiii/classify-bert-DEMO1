a6bcb2f7dab3f968a39a8d34f55745176e85a3a2
Jia-Yiiiii <17640079336@163.com>
2026-06-11 16:52:11 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,7 @@
-
 import json
 import torch
 import random
 import numpy as np
-
-def load_config(config_path):
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = json.load(f)
-    return config
-
 def set_seed(seed=42):
     random.seed(seed)
     np.random.seed(seed)
